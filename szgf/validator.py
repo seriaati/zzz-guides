5c014221f452e83b41b72620751c0d08798c9bc5
seria <seria.ati@gmail.com>
2025-06-23 11:05:11 +0900

    chore: Rename parser to validator

--- szgf/validator.py
+++ szgf/validator.py
@@ -1,7 +1,9 @@
+from __future__ import annotations
+
 from typing import Any
 
 from szgf.schema import Guide
 
 
-def parse_szgf(data: Any) -> Guide:
+def validate_szgf(data: Any) -> Guide:
     return Guide.model_validate(data)
