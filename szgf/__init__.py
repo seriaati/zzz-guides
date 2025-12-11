9f47ff0543f06efa363ccaccd6df8973553c178c
seria <seria.ati@gmail.com>
2025-12-11 16:35:34 +0900

    imp: Expose some imports

--- szgf/__init__.py
+++ szgf/__init__.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+from . import parser, schemas
 from .client import SZGFClient
 from .schemas.original import OriginalGuide
+from .schemas.parsed import ParsedGuide
 from .validator import validate_original_guide
